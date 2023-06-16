5cc79e435b8760578c0cf6d6b4e164549fc0800a
Saurabh Totey <SaurabhTotey@gmail.com>
2023-06-16 15:55:03 -0600

    Attempt GKP implementation

--- src/code.py
+++ src/code.py
@@ -113,6 +113,46 @@ def get_cat_code(symmetry: int, coherent_state_value: complex, squeezing: float,
 	serialize_non_random_code(cat_code)
 	return cat_code
 
+def get_gkp_code(is_hex_lattice: bool, energy_constraint: float, physical_dimension: int) -> Code:
+	code_name = f"gkp-{is_hex_lattice},{energy_constraint},{physical_dimension}"
+	existing_code = deserialize_non_random_code(code_name)
+	if existing_code is not None:
+		return existing_code
+	lowering_operator = qt.destroy(physical_dimension)
+	number_operator = qt.num(physical_dimension)
+	x = (lowering_operator.dag() + lowering_operator) / np.sqrt(2)
+	y = 1j * (lowering_operator.dag() - lowering_operator) / np.sqrt(2)
+	x_displacement = 0
+	z_displacement = 0
+	if is_hex_lattice:
+		x_displacement = np.sqrt(np.pi / np.sqrt(3))
+		z_displacement = np.exp(2j * np.pi / 3) * x_displacement
+	else:
+		x_displacement = np.sqrt(np.pi / 2)
+		z_displacement = 1j * x_displacement
+	sx_gate = qt.displace(physical_dimension, x_displacement) ** 2
+	sz_gate = qt.displace(physical_dimension, z_displacement) ** 2
+	sy_gate = qt.displace(physical_dimension, x_displacement + z_displacement) ** 2
+	h_gate = None
+	clifford_y_gate = None
+	if is_hex_lattice:
+		h_gate = energy_constraint * (x ** 2 + y ** 2) - (sx_gate + sx_gate.dag() + sz_gate + sz_gate.dag() + sy_gate + sy_gate.dag())
+		clifford_y_gate = qt.hadamard_transform() * qt.phasegate(np.pi / 2).dag()
+	else:
+		h_gate = energy_constraint * (x ** 2 + y ** 2) - (sx_gate + sx_gate.dag() + sz_gate + sz_gate.dag())
+		clifford_y_gate = qt.hadamard_transform()
+	_, h_eigenvectors = h_gate.eigenstates()
+	_, c_eigenvectors = clifford_y_gate.eigenstates()
+	ground_state = h_eigenvectors[0]
+	first_excited_state = -h_eigenvectors[1]
+	u = (c_eigenvectors[1] * qt.basis(2, 0).dag() + c_eigenvectors[0] * qt.basis(2, 1).dag()).full()
+	zero_encoding = u[0, 0] * ground_state + u[0, 1] * first_excited_state
+	one_encoding = u[1, 0] * ground_state + u[1, 1] * first_excited_state
+	gkp_code = Code(code_name, (zero_encoding, one_encoding), False)
+	assert_code_is_good(gkp_code)
+	serialize_non_random_code(gkp_code)
+	return gkp_code
+
 def make_projected_haar_random_code(symmetry: int, average_photon_number: int, physical_dimension: int) -> Code:
 	assert symmetry * (average_photon_number + 2) <= physical_dimension
 	zero_projector = sum([qt.ket2dm(qt.basis(physical_dimension, i * symmetry * 2)) for i in range(average_photon_number // 2 + 1)])
